f45d297b5d9c52528f487685d94569203b6700a2
AlexandraM0 <alexandra.moghina97@gmail.com>
2024-02-11 19:26:56 +0200

    functions

--- password-manager.py
+++ password-manager.py
@@ -1,6 +1,10 @@
 pwd = input ("What is the master password? ")
 
-def view ()
+def view ():
+    pass
+
+def add ():
+    pass
     
     while True:
         mode = input (" Whould you like to add a new password or view existing ones (view, add)? If you want to quit press q.").lower()
