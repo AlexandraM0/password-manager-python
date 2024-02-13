b579699784c7c055b90f625c686f674cd597b469
AlexandraM0 <alexandra.moghina97@gmail.com>
2024-02-13 23:12:36 +0200

    Add function

--- password-manager.py
+++ password-manager.py
@@ -8,17 +8,18 @@ def add ():
     password = input ('Password: ')
 
     with open('password.txt', 'a') as f: 
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + password + "\n")
 
-    while True:
-        mode = input (" Whould you like to add a new password or view existing ones (view, add)? If you want to quit press q.").lower()
-        if mode == "q":
-            break
 
-        if mode == "view" :
-            view()
-        elif mode == "add":
-            add()
-        else:
-            print("Invalid mode.")
-            continue
+while True:
+    mode = input (" Whould you like to add a new password or view existing ones (view, add)? If you want to quit press q.").lower()
+    if mode == "q":
+        break
+
+    if mode == "view" :
+         view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
